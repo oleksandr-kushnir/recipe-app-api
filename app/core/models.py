601811cede0e1c4ea956993238ac8fa411a7ad16
oleksandr-kushnir <oleksandr.kushnir1978@gmail.com>
2025-01-01 12:15:24 +0100

    fixed 2

--- app/core/models.py
+++ app/core/models.py
@@ -37,7 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)  # Is able to login to admin area
+    is_staff = models.BooleanField(default=False)  # Allowed to login to admin
 
     objects = UserManager()  # Assingn the User Manager
 
