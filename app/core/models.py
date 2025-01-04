0aea466cb86bc89fa6f3c1fa314ed38d10e60610
oleksandr-kushnir <oleksandr.kushnir1978@gmail.com>
2025-01-04 19:28:29 +0100

    Added Recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Database models.
 """
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -42,3 +43,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()  # Assingn the User Manager
 
     USERNAME_FIELD = "email"  # The field will be used for authentification
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
