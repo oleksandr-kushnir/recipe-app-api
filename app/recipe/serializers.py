c5ab6411d959eddac8df46947b8c817cf91a934a
oleksandr-kushnir <oleksandr.kushnir1978@gmail.com>
2025-02-09 09:39:37 +0100

    Recipe Image API and Filtering

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -96,7 +96,7 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ["description"]
+        fields = RecipeSerializer.Meta.fields + ["description", "image"]
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -106,3 +106,13 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ["id", "name"]
         read_only_fields = ["id"]
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ["id", "image"]
+        read_only_fields = ["id"]
+        extra_kwargs = {"image": {"required": "True"}}
